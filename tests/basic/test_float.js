2b41aad12905c94b5610ad3876c86e3a57cc489b
mohanson <mohanson@outlook.com>
2023-10-17 09:37:19 +0800

    Fix test_float.js

--- tests/basic/test_float.js
+++ tests/basic/test_float.js
@@ -4,7 +4,12 @@ function assert(actual, expected, message) {
     if (arguments.length == 1)
         expected = true;
 
-    if (actual == expected)
+    if (actual === expected)
+        return;
+
+    if (actual !== null && expected !== null
+    &&  typeof actual == 'object' && typeof expected == 'object'
+    &&  actual.toString() === expected.toString())
         return;
 
     throw Error("assertion failed: got |" + actual + "|" +
@@ -14,7 +19,7 @@ function assert(actual, expected, message) {
 
 function test_to_string() {
     assert(1.1.toString(), "1.1")
-    assert(-1.1.toString(), "-1.100000")
+    assert((-1.1).toString(), "-1.1")
     assert(3.14159265354.toString(), "3.1415926540000000")
 }
 
