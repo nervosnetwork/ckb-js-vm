db805ce7db997f9cd1ca9f01cd715a00ba6bc69b
mohanson <mohanson@outlook.com>
2024-09-11 12:21:36 +0800

    Use awk to rewrite compile.lua

--- tools/compile.lua
+++
@@ -1,8 +0,0 @@
-for line in io.lines() do
-    if line:find("^Script log: ") ~= nil then
-        local line_data = line:gsub("^Script log: ", "")
-        if line_data ~= "" then
-            print(line_data)
-        end
-    end
-end
