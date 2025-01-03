e5d8ae3726e9a8778622706c1d1e6240c4ea268d
Lyndon <lynndon@gmail.com>
2025-01-03 16:29:24 +0800

    fix: dump all data instead of hash

--- src/dump_secp256k1_data.c
+++ src/dump_secp256k1_data.c
@@ -13,46 +13,47 @@
 #define ERROR_IO -1
 
 int main(int argc, char* argv[]) {
-  size_t pre_size = sizeof(secp256k1_pre_g);
-  size_t pre128_size = sizeof(secp256k1_pre_g_128);
+    size_t pre_size = sizeof(secp256k1_pre_g);
+    size_t pre128_size = sizeof(secp256k1_pre_g_128);
 
-  FILE* fp_data = fopen("build/secp256k1_data", "wb");
-  if (!fp_data) {
-    return ERROR_IO;
-  }
-  fwrite(secp256k1_pre_g, pre_size, 1, fp_data);
-  fwrite(secp256k1_pre_g_128, pre128_size, 1, fp_data);
-  fclose(fp_data);
-
-  FILE* fp = fopen("build/secp256k1_data_info.h", "w");
-  if (!fp) {
-    return ERROR_IO;
-  }
-
-  fprintf(fp, "#ifndef CKB_SECP256K1_DATA_INFO_H_\n");
-  fprintf(fp, "#define CKB_SECP256K1_DATA_INFO_H_\n");
-  fprintf(fp, "#define CKB_SECP256K1_DATA_SIZE %ld\n", pre_size + pre128_size);
-  fprintf(fp, "#define CKB_SECP256K1_DATA_PRE_SIZE %ld\n", pre_size);
-  fprintf(fp, "#define CKB_SECP256K1_DATA_PRE128_SIZE %ld\n", pre128_size);
-
-  blake2b_state blake2b_ctx;
-  uint8_t hash[32];
-  blake2b_init(&blake2b_ctx, 32);
-  blake2b_update(&blake2b_ctx, secp256k1_pre_g, pre_size);
-  blake2b_update(&blake2b_ctx, secp256k1_pre_g_128,
-                 pre128_size);
-  blake2b_final(&blake2b_ctx, hash, 32);
-
-  fprintf(fp, "static uint8_t ckb_secp256k1_data_hash[32] = {\n  ");
-  for (int i = 0; i < 32; i++) {
-    fprintf(fp, "%u", hash[i]);
-    if (i != 31) {
-      fprintf(fp, ", ");
+    FILE* fp_data = fopen("build/secp256k1_data", "wb");
+    if (!fp_data) {
+        return ERROR_IO;
     }
-  }
-  fprintf(fp, "\n};\n");
-  fprintf(fp, "#endif\n");
-  fclose(fp);
+    fwrite(secp256k1_pre_g, pre_size, 1, fp_data);
+    fwrite(secp256k1_pre_g_128, pre128_size, 1, fp_data);
+    fclose(fp_data);
 
-  return 0;
+    FILE* fp = fopen("build/secp256k1_data_info.h", "w");
+    if (!fp) {
+        return ERROR_IO;
+    }
+
+    fprintf(fp, "#ifndef CKB_SECP256K1_DATA_INFO_H_\n");
+    fprintf(fp, "#define CKB_SECP256K1_DATA_INFO_H_\n");
+    fprintf(fp, "#define CKB_SECP256K1_DATA_SIZE %ld\n", pre_size + pre128_size);
+    fprintf(fp, "#define CKB_SECP256K1_DATA_PRE_SIZE %ld\n", pre_size);
+    fprintf(fp, "#define CKB_SECP256K1_DATA_PRE128_SIZE %ld\n", pre128_size);
+
+    fprintf(fp, "static uint8_t ckb_secp256k1_data[] = {\n  ");
+    unsigned char* p = (unsigned char*)secp256k1_pre_g;
+    for (int i = 0; i < pre_size; i++) {
+        fprintf(fp, "0x%02x", p[i]);
+        fprintf(fp, ", ");
+    }
+    fprintf(fp, "\n");
+    p = (unsigned char*)secp256k1_pre_g_128;
+    for (int i = 0; i < pre128_size; i++) {
+        fprintf(fp, "0x%02x", p[i]);
+        if (i != (pre128_size - 1)) {
+            fprintf(fp, ", ");
+        }
+    }
+
+    fprintf(fp, "\n};\n");
+
+    fprintf(fp, "#endif\n");
+    fclose(fp);
+
+    return 0;
 }
