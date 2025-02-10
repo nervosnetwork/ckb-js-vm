06026d1b4c03ec9f3cb923c017b9c64561c4e79b
Lyndon <lynndon@gmail.com>
2025-02-10 14:17:00 +0800

    refactor: use map and iterator

--- packages/examples/src/simple_udt.ts
+++ packages/examples/src/simple_udt.ts
@@ -19,30 +19,36 @@ function main() {
     }
   }
 
-  let input_amount = 0n;
-  for (let data of new HighLevel.QueryIter(
-    bindings.loadCellData,
-    bindings.SOURCE_GROUP_INPUT,
-  )) {
-    if (data.byteLength != 16) {
-      throw `Invalid data length: ${data.byteLength}`;
-    }
-    let n = new BigUint64Array(data);
-    let current_amount = n[0] | (n[1] << 64n);
-    input_amount += current_amount;
-  }
-  let output_amount = 0n;
-  for (let data of new HighLevel.QueryIter(
-    bindings.loadCellData,
-    bindings.SOURCE_GROUP_OUTPUT,
-  )) {
-    if (data.byteLength != 16) {
-      throw `Invalid data length: ${data.byteLength}`;
-    }
-    let n = new BigUint64Array(data);
-    let current_amount = n[0] | (n[1] << 64n);
-    output_amount += current_amount;
-  }
+  let input_amount = [
+    ...new HighLevel.QueryIter(
+      bindings.loadCellData,
+      bindings.SOURCE_GROUP_INPUT,
+    ),
+  ]
+    .map((data) => {
+      if (data.byteLength != 16) {
+        throw `Invalid data length: ${data.byteLength}`;
+      }
+      const n = new BigUint64Array(data);
+      return n[0] | (n[1] << 64n);
+    })
+    .reduce((sum, amount) => sum + amount, 0n);
+
+  let output_amount = [
+    ...new HighLevel.QueryIter(
+      bindings.loadCellData,
+      bindings.SOURCE_GROUP_OUTPUT,
+    ),
+  ]
+    .map((data) => {
+      if (data.byteLength != 16) {
+        throw `Invalid data length: ${data.byteLength}`;
+      }
+      const n = new BigUint64Array(data);
+      return n[0] | (n[1] << 64n);
+    })
+    .reduce((sum, amount) => sum + amount, 0n);
+
   log.debug(`verifying amount: ${input_amount} and ${output_amount}`);
   if (input_amount < output_amount) {
     return -1;
