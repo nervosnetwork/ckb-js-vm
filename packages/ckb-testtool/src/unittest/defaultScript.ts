b26697ab13965841fd08e98fb7496f81bbcd97a4
Lyndon <lynndon@gmail.com>
2025-03-03 15:50:19 +0800

    feat: add CKB JS VM default script for testing

--- packages/ckb-testtool/src/unittest/defaultScript.ts
+++ packages/ckb-testtool/src/unittest/defaultScript.ts
@@ -8,3 +8,9 @@ export const DEFAULT_SCRIPT_ALWAYS_FAILURE = path.join(
   __dirname,
   "../../src/unittest/defaultScript/alwaysFailure",
 );
+
+export const DEFAULT_SCRIPT_CKB_JS_VM = path.join(
+  __dirname,
+  "../../src/unittest/defaultScript/ckb-js-vm",
+);
+
